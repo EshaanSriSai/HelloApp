f86d3e8e58cfa21f4d1ed25d388b16244258ad41
EshaanSriSai <eshaansrisai@gmail.com>
2026-04-13 19:48:36 +0530

    UC6: Use substring method to remove trailing comma

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,21 +1,20 @@
 public class HelloWorldApp {
-
     public static void main(String[] args) {
 
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            System.out.print("Hello ");
 
-            boolean first = true;
+            String result = "";
 
             for (String name : args) {
-                if (!first) {
-                    System.out.print(", ");
-                }
-                System.out.print(name);
-                first = false;
+                result += name + ", ";
             }
+
+            // remove last ", "
+            result = result.substring(0, result.length() - 2);
+
+            System.out.println("Hello, " + result + "!");
         }
     }
 }
